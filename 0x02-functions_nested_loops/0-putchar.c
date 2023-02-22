2fa212054dea94aed40285bd88d1f76b6615c771
Johnson Kamau <johnsonkamau155@gmail.com>
2023-02-22 11:05:27 -0800

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,40 +1,26 @@
 #include "main.h"
 
-        /**
+/**
 
-         * main - print _putchar
+* main - print _putchar
 
-         * is written by mbah
+* is written by Johnson
 
-         * Return: 0
+* Return: 0
 
-         */
+*/
 
-
-
-        int main(void)
-
-        {
-
-        _putchar('_');
-
+int main(void)
+{
+       	_putchar('_');
         _putchar('p');
-
         _putchar('u');
-
         _putchar('t');
-
         _putchar('c');
-
         _putchar('h');
-
         _putchar('a');
-
         _putchar('r');
-
         _putchar('\n');
 
         return (0);
-
-        }
-
+}
