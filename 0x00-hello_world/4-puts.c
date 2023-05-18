c3b655e02391f300a93fdfa67c238135f27a77fc
itskamau <johnsonkamau155@gmail.com>
2023-05-18 14:23:41 -0700

    Add C program to print string using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
+/**
+ * main- entry poit of the program
+ *
+ * This function is the entry poit of the program
+ * It prints the message 'Programming is like a multilingual puzzle'
+ * Uses the puts function and resturns zero to show successful execution
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
